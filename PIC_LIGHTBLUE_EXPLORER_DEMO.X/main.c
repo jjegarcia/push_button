ce15ba6de29e188e1c325e5d33814c947ae268c8
Jenaro Garcia <jjegarcia@hotmail.com>
2022-12-08 19:54:28 +0000

    added connection conditional

--- PIC_LIGHTBLUE_EXPLORER_DEMO.X/main.c
+++ PIC_LIGHTBLUE_EXPLORER_DEMO.X/main.c
@@ -104,114 +104,81 @@ int main(void) {
     //    RESET_TIMER_INTERRUPT_FLAG;
 
     while (1) {
-        switch (state) {
-            case LISTEN:
-                if (IS_BUTTON_ACTIVE()) {
-                    BUTTON_ACTIVE_CLEAR();
-                    if (!IS_BUTTON_INITIALISED()) {
-                        BUTTON_INITIALISED();
-                    } else {
-                        DataLedOn();
-                        EXT_INT_risingEdgeSet();
+        if (RN487X_IsConnected() == true) {
+            switch (state) {
+                case LISTEN:
+                    if (IS_BUTTON_ACTIVE()) {
+                        BUTTON_ACTIVE_CLEAR();
+                        if (!IS_BUTTON_INITIALISED()) {
+                            BUTTON_INITIALISED();
+                        } else {
+                            DataLedOn();
+                            EXT_INT_risingEdgeSet();
+                            RELOAD_TIMER();
+                            RESET_TIMER_INTERRUPT_FLAG;
+                            START_TIMER();
+                            state = PUSHED;
+                        }
+                    }
+                    break;
+                case PUSHED:
+                    if (TIMER_FLAG_SET() && !IS_BUTTON_ACTIVE()) {
+                        FAST_MODE();
                         RELOAD_TIMER();
                         RESET_TIMER_INTERRUPT_FLAG;
-                        START_TIMER();
-                        state = PUSHED;
-                    }
-                }
-                break;
-            case PUSHED:
-                if (TIMER_FLAG_SET() && !IS_BUTTON_ACTIVE()) {
-                    FAST_MODE();
-                    RELOAD_TIMER();
-                    RESET_TIMER_INTERRUPT_FLAG;
-                    BUTTON_ACTIVE_CLEAR();
-                    EXT_INT_fallingEdgeSet();
-                    state = TIMEOUT;
-                } else {
-                    if (IS_BUTTON_ACTIVE()) {
-                        DataLedOff();
+                        BUTTON_ACTIVE_CLEAR();
                         EXT_INT_fallingEdgeSet();
-                        BUTTON_ACTIVE_CLEAR();
-                        state = LISTEN;
-                    }
-                }
-                break;
-            case TIMEOUT:
-                if (TIMER_FLAG_SET()) {
-                    blink();
-                    if (IS_BUTTON_ACTIVE()) {
-                        BUTTON_ACTIVE_CLEAR();
-                        EXT_INT_risingEdgeSet();
-                        counter = 0;
-                        state = COMPLETE;
-                    }
-                }
-                break;
-            case COMPLETE:
-                counter++;
-                blink();
-                bool test = IS_BUTTON_ACTIVE();
-                if (counter == COUNTER_THRESHOLD && !IS_BUTTON_ACTIVE()) {
-                    BUTTON_ACTIVE_CLEAR();
-                    EXT_INT_fallingEdgeSet();
-                    //                    counter = 0;
-                    state = RESET;
-                } else {
-                    if (IS_BUTTON_ACTIVE()) {
-                        EXT_INT_fallingEdgeSet();
-                        BUTTON_ACTIVE_CLEAR();
                         state = TIMEOUT;
+                    } else {
+                        if (IS_BUTTON_ACTIVE()) {
+                            DataLedOff();
+                            EXT_INT_fallingEdgeSet();
+                            BUTTON_ACTIVE_CLEAR();
+                            state = LISTEN;
+                        }
                     }
-                }
-                break;
-            case RESET:
-                //                    EXT_INT_risingEdgeSet();
-                SLOW_MODE();
-                BUTTON_ACTIVE_CLEAR();
-                DataLedOff();
-                state = LISTEN;
-                break;
-
-
-
-
-
-                //        if (RN487X_IsConnected() == true) {
-                //            if (TIMER_FLAG_SET() == true) {
-                //                RESET_TIMER_INTERRUPT_FLAG;
-                //
-                //                LIGHTBLUE_TemperatureSensor();
-                //                LIGHTBLUE_AccelSensor();
-                //                LIGHTBLUE_PushButton();
-                //                LIGHTBLUE_LedState();
-                //                LIGHTBLUE_SendProtocolVersion();
-                //            } else {
-                //                while (RN487X_DataReady()) {
-                //                    LIGHTBLUE_ParseIncomingPacket(RN487X_Read());
-                //                }
-                //                while (uart[UART_CDC].DataReady()) {
-                //                    lightBlueSerial[serialIndex] = uart[UART_CDC].Read();
-                //                    if ((lightBlueSerial[serialIndex] == '\r')
-                //                            || (lightBlueSerial[serialIndex] == '\n')
-                //                            || (serialIndex == (sizeof (lightBlueSerial) - 1))) {
-                //                        lightBlueSerial[serialIndex] = '\0';
-                //                        LIGHTBLUE_SendSerialData(lightBlueSerial);
-                //                        serialIndex = 0;
-                //                    } else {
-                //                        serialIndex++;
-                //                    }
-                //                }
-                //
-                //            }
-                //        } else {
-                //            while (RN487X_DataReady()) {
-                //                uart[UART_CDC].Write(RN487X_Read());
-                //            }
-                //            while (uart[UART_CDC].DataReady()) {
-                //                RN487X.Write(uart[UART_CDC].Read());
-                //            }
-                //        }
+                    break;
+                case TIMEOUT:
+                    if (TIMER_FLAG_SET()) {
+                        blink();
+                        if (IS_BUTTON_ACTIVE()) {
+                            BUTTON_ACTIVE_CLEAR();
+                            EXT_INT_risingEdgeSet();
+                            counter = 0;
+                            state = COMPLETE;
+                        }
+                    }
+                    break;
+                case COMPLETE:
+                    counter++;
+                    blink();
+                    bool test = IS_BUTTON_ACTIVE();
+                    if (counter == COUNTER_THRESHOLD && !IS_BUTTON_ACTIVE()) {
+                        BUTTON_ACTIVE_CLEAR();
+                        EXT_INT_fallingEdgeSet();
+                        state = RESET;
+                    } else {
+                        if (IS_BUTTON_ACTIVE()) {
+                            EXT_INT_fallingEdgeSet();
+                            BUTTON_ACTIVE_CLEAR();
+                            state = TIMEOUT;
+                        }
+                    }
+                    break;
+                case RESET:
+                    SLOW_MODE();
+                    BUTTON_ACTIVE_CLEAR();
+                    DataLedOff();
+                    state = LISTEN;
+                    break;
+            }
+        } else {
+            while (RN487X_DataReady()) {
+                uart[UART_CDC].Write(RN487X_Read());
+            }
+            while (uart[UART_CDC].DataReady()) {
+                RN487X.Write(uart[UART_CDC].Read());
+            }
         }
     }
     return 0;
