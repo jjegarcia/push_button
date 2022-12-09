2816b1053b16ebe13222aefa3a39392a35185a07
Jenaro Garcia <jjegarcia@hotmail.com>
2022-12-09 08:25:11 +0000

    set functions

--- PIC_LIGHTBLUE_EXPLORER_DEMO.X/main.c
+++ PIC_LIGHTBLUE_EXPLORER_DEMO.X/main.c
@@ -67,6 +67,11 @@ static char statusBuffer[MAX_BUFFER_SIZE]; /**< Status Buffer instance passed to
 static char lightBlueSerial[MAX_BUFFER_SIZE]; /**< Message Buffer used for CDC Serial communication when connected. Terminated by \r, \n, MAX character Passes messages to BLE for transmisison. */
 static uint8_t serialIndex; /**< Local index value for serial communication buffer. */
 static bool LED_ON;
+void setListen(void);
+void setPushed(void);
+void setTimeout(void);
+void setComplete(void);
+void setReset(void);
 void blink(void);
 
 #define IS_LED_ON()     (LED_ON==true)
@@ -81,13 +86,15 @@ typedef enum {
     RESET
 } _states;
 
+_states state = LISTEN;
+int counter = 0;
+
 /*
                      Main application
  */
 int main(void) {
     int COUNTER_THRESHOLD = 99999999999;
-    _states state = LISTEN;
-    int counter = 0;
+
     // initialize the device
     SYSTEM_Initialize();
     RN487X_SetAsyncMessageHandler(statusBuffer, sizeof (statusBuffer));
@@ -104,84 +111,103 @@ int main(void) {
     //    RESET_TIMER_INTERRUPT_FLAG;
 
     while (1) {
-        if (RN487X_IsConnected() == true) {
-            switch (state) {
-                case LISTEN:
-                    if (IS_BUTTON_ACTIVE()) {
-                        BUTTON_ACTIVE_CLEAR();
-                        if (!IS_BUTTON_INITIALISED()) {
-                            BUTTON_INITIALISED();
-                        } else {
-                            DataLedOn();
-                            EXT_INT_risingEdgeSet();
-                            RELOAD_TIMER();
-                            RESET_TIMER_INTERRUPT_FLAG;
-                            START_TIMER();
-                            state = PUSHED;
-                        }
-                    }
-                    break;
-                case PUSHED:
-                    if (TIMER_FLAG_SET() && !IS_BUTTON_ACTIVE()) {
-                        FAST_MODE();
-                        RELOAD_TIMER();
-                        RESET_TIMER_INTERRUPT_FLAG;
-                        BUTTON_ACTIVE_CLEAR();
-                        EXT_INT_fallingEdgeSet();
-                        state = TIMEOUT;
-                    } else {
-                        if (IS_BUTTON_ACTIVE()) {
-                            DataLedOff();
-                            EXT_INT_fallingEdgeSet();
-                            BUTTON_ACTIVE_CLEAR();
-                            state = LISTEN;
-                        }
-                    }
-                    break;
-                case TIMEOUT:
-                    if (TIMER_FLAG_SET()) {
-                        blink();
-                        if (IS_BUTTON_ACTIVE()) {
-                            BUTTON_ACTIVE_CLEAR();
-                            EXT_INT_risingEdgeSet();
-                            counter = 0;
-                            state = COMPLETE;
-                        }
-                    }
-                    break;
-                case COMPLETE:
-                    counter++;
-                    blink();
-                    bool test = IS_BUTTON_ACTIVE();
-                    if (counter == COUNTER_THRESHOLD && !IS_BUTTON_ACTIVE()) {
-                        BUTTON_ACTIVE_CLEAR();
-                        EXT_INT_fallingEdgeSet();
-                        state = RESET;
-                    } else {
-                        if (IS_BUTTON_ACTIVE()) {
-                            EXT_INT_fallingEdgeSet();
-                            BUTTON_ACTIVE_CLEAR();
-                            state = TIMEOUT;
-                        }
-                    }
-                    break;
-                case RESET:
-                    SLOW_MODE();
+        //        if (RN487X_IsConnected() == true) {
+        switch (state) {
+            case LISTEN:
+                if (IS_BUTTON_ACTIVE()) {
                     BUTTON_ACTIVE_CLEAR();
-                    DataLedOff();
-                    state = LISTEN;
-                    break;
-            }
-        } else {
-            while (RN487X_DataReady()) {
-                uart[UART_CDC].Write(RN487X_Read());
-            }
-            while (uart[UART_CDC].DataReady()) {
-                RN487X.Write(uart[UART_CDC].Read());
-            }
+                    if (!IS_BUTTON_INITIALISED()) {
+                        BUTTON_INITIALISED();
+                    } else {
+                        setPushed();
+                    }
+                }
+                break;
+            case PUSHED:
+                if (TIMER_FLAG_SET() && !IS_BUTTON_ACTIVE()) {
+                    setTimeout();
+                } else {
+                    if (IS_BUTTON_ACTIVE()) {
+                        setListen();
+                    }
+                }
+                break;
+            case TIMEOUT:
+                if (TIMER_FLAG_SET()) {
+                    blink();
+                    if (IS_BUTTON_ACTIVE()) {
+                        setComplete();
+                    }
+                }
+                break;
+            case COMPLETE:
+                counter++;
+                blink();
+                bool test = IS_BUTTON_ACTIVE();
+                if (counter == COUNTER_THRESHOLD && !IS_BUTTON_ACTIVE()) {
+                    setReset();
+                } else {
+                    if (IS_BUTTON_ACTIVE()) {
+                        setTimeout();
+                        //                        EXT_INT_fallingEdgeSet();
+                        //                        BUTTON_ACTIVE_CLEAR();
+                        //                        state = TIMEOUT;
+                    }
+                }
+                break;
+            case RESET:
+                setListen();
+                break;
         }
+        //        } else {
+        //            while (RN487X_DataReady()) {
+        //                uart[UART_CDC].Write(RN487X_Read());
+        //            }
+        //            while (uart[UART_CDC].DataReady()) {
+        //                RN487X.Write(uart[UART_CDC].Read());
+        //            }
+        //        }
     }
     return 0;
+}
+
+void setListen(void) {
+    DataLedOff();
+    BUTTON_ACTIVE_CLEAR();
+    SLOW_MODE();
+    EXT_INT_fallingEdgeSet();
+    state = LISTEN;
+}
+
+void setPushed(void) {
+    DataLedOn();
+    EXT_INT_risingEdgeSet();
+    RELOAD_TIMER();
+    RESET_TIMER_INTERRUPT_FLAG;
+    START_TIMER();
+    state = PUSHED;
+}
+
+void setTimeout(void) {
+    FAST_MODE();
+    RELOAD_TIMER();
+    RESET_TIMER_INTERRUPT_FLAG;
+    BUTTON_ACTIVE_CLEAR();
+    EXT_INT_fallingEdgeSet();
+    state = TIMEOUT;
+}
+
+void setComplete(void) {
+    BUTTON_ACTIVE_CLEAR();
+    EXT_INT_risingEdgeSet();
+    counter = 0;
+    state = COMPLETE;
+}
+
+void setReset(void) {
+    BUTTON_ACTIVE_CLEAR();
+    EXT_INT_fallingEdgeSet();
+    state = RESET;
 }
 
 void blink(void) {
